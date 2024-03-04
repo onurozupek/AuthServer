892d2bc7dbcf90503bd45c329ac0934c285055dd
Onur Özüpek <onur.ozupek@acibadem.com>
2024-03-04 16:28:07 +0300

    Add project files.

---
+++ AuthServer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        IQueryable<TEntity> Where(Expression<Func<TEntity,bool>> predicate);
+
+        //product = productRepository.where(x=>x.id>5); id si 5 ten büyük olanlar geldi
+        //Henüz db ye yansımadı
+        //product.ToList dediğin zaman tek bir sorguya dönüştürülüp çağırılır o zaman db ye yansır.
+
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+    }
+}
